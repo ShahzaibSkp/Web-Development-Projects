c316051c02a5e40829646168782ca8ea38c7521b
Muhammad Shahzaib Afzal <shahzaibafzal772@gmail.com>
2026-07-29 14:37:35 +0500

    New Mini Project (Guess Number Game)

---
+++ Mini Projects/Guess Number Game/script.js
@@ -0,0 +1,51 @@
+let inputValue = document.getElementById("userInput");
+let submitBtn = document.getElementById("submitBtn");
+let attemptCtn = document.getElementById("attempts");
+let msg = document.getElementById("message");
+
+const rndNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
+let isFinished = false;
+
+submitBtn.addEventListener("click", () => {
+    const userGuess = Number(inputValue.value);
+
+    if (isFinished) return;
+
+    if (userGuess <= 0 || userGuess > 100 || userGuess == "") {
+        msg.textContent = "⚠ Please Enter a valid Number!";
+        msg.style.color = "red";
+
+        return;
+    }
+
+    attempts++;
+    document.getElementById("attempts").textContent = `Attempts: ${attempts}`;
+
+    if (userGuess === rndNumber) {
+        msg.textContent = `Success 🎉! You guessed the correct number (${rndNumber}) in ${attempts} attempts!`;
+        msg.style.color = "green";
+
+        inputValue.disabled = true;
+        submitBtn.disabled = true;
+
+        const reload = document.createElement("p");
+        reload.setAttribute("id", "reloadText");
+        reload.innerHTML = "Click <span>here</span> to play again.";
+        document.querySelector(".output-group").appendChild(reload);
+
+        document.querySelector("span").addEventListener('click', () => {
+            location.reload();
+        });
+    }
+
+    else if (userGuess > rndNumber) {
+        msg.textContent = "Too High 📈! Try again.";
+        msg.style.color = "red";
+    }
+
+    else {
+        msg.textContent = "Too Low 📉! Try again.";
+        msg.style.color = "red";
+    }
+})
